e7236411ce6036ae5cf933712453ca2920b28263
Ali Khatib <alikhatib2004@gmail.com>
2025-03-05 16:25:07 -0500

    update

---
+++ purpletone.js
@@ -0,0 +1,9 @@
+t.prefs_.set('color-palette-overrides', [
+  "#1E0A2A", "#7A3EFF", "#8A64FF", "#A08CFF",
+  "#9F64FF", "#5E3CB0", "#3BB1FF", "#E6DAFF",
+  "#251133", "#AA7BFF", "#8B75FF", "#B299FF",
+  "#8C70FF", "#7E62B3", "#6096BF", "#F7EDFF"
+]);
+t.prefs_.set('foreground-color', "#E6DAFF");
+t.prefs_.set('background-color', "#000000");
+t.prefs_.set('cursor-color', "rgba(158,158,203,0.5)");
